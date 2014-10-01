02c33d624a63c7d5fa34f116b2209c816d4564da
Michael D. Lowis <mike.lowis@gentex.com>
2014-10-01 14:03:46 -0400

    Updated AR utility for clang

--- toolsets/clang.rb
+++ toolsets/clang.rb
@@ -1,6 +1,7 @@
 module Toolsets
   def self.clang(env, options = {})
     env.set_toolset(:gcc, options)
+    env['AR']  = 'llvm-ar'
     env['AS']  = 'clang'
     env['CC']  = 'clang'
     env['CXX'] = 'clang'
