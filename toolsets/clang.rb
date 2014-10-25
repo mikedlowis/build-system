727e6f5ffd84778e13f16fffafbc42a987dd1796
Michael D. Lowis <mike@mdlowis.com>
2014-10-25 19:56:51 -0400

    Removed AR definition for clang

--- toolsets/clang.rb
+++ toolsets/clang.rb
@@ -1,7 +1,6 @@
 module Toolsets
   def self.clang(env, options = {})
     env.set_toolset(:gcc, options)
-    env['AR']  = 'llvm-ar'
     env['AS']  = 'clang'
     env['CC']  = 'clang'
     env['CXX'] = 'clang'
